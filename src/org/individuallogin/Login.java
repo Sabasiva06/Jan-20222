24b5917d496ba3d333f10afa40fbf052c7dc341d
Sabasiva06 <sabasiv06@gmail.com>
2022-02-02 08:09:26 +0530

    siva works

--- src/org/individuallogin/Login.java
+++ src/org/individuallogin/Login.java
@@ -1,33 +1,43 @@
 package org.individuallogin;
 
-public class Login {             //class
+public class Login { // class
 
-	private void username() {    //method
-		
+	private void username() { // method
+
 		System.out.println("ramsarath01@gmail.com");
 
 	}
-	
-    private void password() {    //method
-    	
-    	System.out.println("1234567890");
-    }
-	
-    private void loginButton() {  //method
-		
-    	System.out.println("click login button");
+
+	private void password() { // method
+
+		System.out.println("1234567890");
+	}
+
+	private void loginButton() { // method
+
+		System.out.println("click login button");
 
 	}
-	
-    public static void main(String[] args) {           //main method
-    	
-		//object creation - ClassName objectName = new ClassName();
-    	Login l = new Login();
-    	
-    	//method call - objectName.methodName();
-    	l.loginButton();
-    	l.username();
-    	l.password();
-    	
-    }
+
+	private void b1() {
+		System.out.println("method b1");
+
+	}
+
+	private void b2() {
+		System.out.println("method b2");
+
+	}
+
+	public static void main(String[] args) { // main method
+
+		// object creation - ClassName objectName = new ClassName();
+		Login l = new Login();
+
+		// method call - objectName.methodName();
+		l.loginButton();
+		l.username();
+		l.password();
+
+	}
 }
